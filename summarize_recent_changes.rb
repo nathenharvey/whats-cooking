15b1e395fc2a06760b0b67e810a82393fc180b04
Nathen Harvey <nathen.harvey@gmail.com>
2012-03-10 16:43:52 -0500

    update the summarize script to help get started on the news page

--- summarize_recent_changes.rb
+++ summarize_recent_changes.rb
@@ -5,11 +5,31 @@ require 'net/http'
 
 
 @con = Mongo::Connection.new
-@con.drop_database('opscode')
 @db = @con['opscode']
 @cookbooks = @db['cookbooks']
 
-lastest_cookbooks = @cookbooks.find(
+lastest_cookbooks = @cookbooks.find.sort( [["updated_at", -1 ]] ).limit(50)
+open_cookbooks = []
+open_users = []
+lastest_cookbooks.each do |cb|
+  maintainer = cb["cookbook_maintainer"]
+  unless maintainer == "opscode"
+    open_cookbooks << cb
+    open_users << cb
+    name = cb["name"]
+    maintainer = cb["cookbook_maintainer"]
+    url = "http://community.opscode.com/cookbooks/" + name
+    description = cb["description"]
+    updated_at = cb["updated_at"]
+    latest_version = cb["versions"][0].split("/").pop.gsub("_",".")
+    puts "### [#{name}](#{url}) v#{latest_version}"
+    puts " * [#{maintainer}](http://community.opscode.com/users/#{maintainer})"
+    puts " * #{description}"
+    puts " * #{updated_at}"
+  end
+end
 
-puts "Find the latest cookbooks using db.cookbooks.find({}, {cookbook_name: 1, cookbook_maintainer:1,updated_at:1}).sort({updated_at:-1})"
-
+open_cookbooks.each do |open|
+  puts "open http://community.opscode.com/cookbooks/#{open["name"]}"
+  puts "open http://community.opscode.com/users/#{open["cookbook_maintainer"]}"
+end
